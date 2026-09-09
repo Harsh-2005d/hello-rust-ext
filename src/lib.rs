7ced1408cffbb8636ccda53bad3e5c73374c50f9
Harsh Dahiya <dahiyaharsh2005@gmail.com>
2026-09-09 18:04:02 +0530

    final

--- src/lib.rs
+++ src/lib.rs
@@ -4,36 +4,10 @@ use std::cmp::Ordering;
 use std::io;
 
 #[pyfunction]
-fn guess_the_number() {
-    println!("Guess the number!");
-
+fn get_number() {
+    
     let secret_number = rand::rng().random_range(1..101);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    println!("your number is {}", secret_number);
 }
 
 /// A Python module implemented in Rust. The name of this function must match
@@ -41,7 +15,7 @@ fn guess_the_number() {
 /// import the module.
 #[pymodule]
 fn hello(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(guess_the_number, m)?)?;
+    m.add_function(wrap_pyfunction!(get_number, m)?)?;
 
     Ok(())
 }
